9c206bca6b986e194df2626a2dc9d2e6448a358a
dmitry.schadrin21@yandex.ru <dmitry.schadrin21@yandex.ru>
2025-12-01 21:11:02 +0300

    Delete redundant code

--- src/api/_app.py
+++ src/api/_app.py
@@ -4,19 +4,14 @@ from fastapi import FastAPI
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 
-from app_context import limiter_var
-
 from .analytics import router as analytics_router
 from .products import router as products_router
 from .users import router as users_router
 
 
 def create_api_app(main_app_state: Any) -> FastAPI:
-    limiter = limiter_var.get()
-
     app = FastAPI()
 
-    app.state.limiter = limiter
     app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)  # type: ignore
 
     app.state.main_app = main_app_state
